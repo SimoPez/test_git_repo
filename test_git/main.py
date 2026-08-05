82bf9df0f30029e91ce6cf8b6312152d723b9289
SimoPez <67878438+SimoPez@users.noreply.github.com>
2026-08-05 21:43:35 +0200

    Modify main.py to save verso() output to file
    Collect output from verso() and write to versi.txt

--- test_git/main.py
+++ test_git/main.py
@@ -7,5 +7,9 @@ if __name__ == 'main':
     parser.add_argument("-cnt", "--count")
     args = parser.parse_args()
 
+    to_print = ''
     for c in int(args.count):
-        print(verso())
+        to_print = to_print + verso() + '\n'
+    
+    with open('versi.txt', 'w') as f:
+        f.write(to_print)
