f5149f8725453bffd3636afcb7caa93ec41f4af3
SimoPez <67878438+SimoPez@users.noreply.github.com>
2026-08-05 22:01:09 +0200

    Fix main guard and update loop for count argument

--- test_git/main.py
+++ test_git/main.py
@@ -1,14 +1,14 @@
 import argparse
 from test_git_utils.verso import verso
 
-if __name__ == 'main':
+if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-cnt", "--count")
     args = parser.parse_args()
 
     to_print = ''
-    for c in int(args.count):
+    for c in range(int(args.count)):
         to_print = to_print + verso() + '\n'
     
     with open('/Users/jim/My_Projects/versi.txt', 'w') as f:
